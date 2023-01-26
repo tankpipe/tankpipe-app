eed786c00feab646850a8ad0348a07e9caf0deca
Kremmen <you@example.com>
2023-01-26 16:35:30 +1000

    remove ally warnings

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+const config = {
+    onwarn: (warning, handler) => {
+      if (warning.code.startsWith('a11y-')) {
+        return;
+      }
+      handler(warning);
+    },
+    kit: {
+      adapter: adapter(),
+    }
+};
