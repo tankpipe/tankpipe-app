e021ce4d51deab6fae6287f97fd512b81122df16
Kremmen <you@example.com>
2023-04-21 18:56:34 +1000

    Remove debug log

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ import { writable } from 'svelte/store'
 const config = writable({})
 
 const dateFormat = (config) => {
-    console.log("*** " + config.display_date_format)
     switch (config.display_date_format) {
         case "Regular": return "dd/MM/yyyy"
         case "US": return "MM/dd/yyyy"
