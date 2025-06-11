024762095d376a74ae2ea1766b2fdf264b5e93eb
Kremmen <you@example.com>
2025-06-11 14:29:19 +1000

    Add Journal view test

--- tests/Transactions.test.js
+++ tests/Transactions.test.js
@@ -39,8 +39,14 @@ it('is displayed correctly for ISO date', async () => {
     await checkResults(mockFetchTransactions)
 });
 
+it('is displayed correctly for Journal view', async () => {
+    const mockFetchTransactions = loadTransactions()
+    config.set({display_date_format: "Regular"})
+    await checkResults(mockFetchTransactions)
+});
+
 async function checkResults(mockFetchTransactions) {
-    const { findByText, container } = render(Transactions, { curAccount: account_data[0] })
+    const { findByText, container } = render(Transactions, { curAccount: account_data[0], journalMode: true })
     const _waitForRenderUpdate = await findByText('Description')
     // Note: Select should show Account 1 as selected but defaults to Account 3
     expect(container.outerHTML).toMatchSnapshot()
