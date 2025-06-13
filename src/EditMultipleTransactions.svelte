061e8655c88dfcf417b491074184ea0a6b39f726
Kremmen <you@example.com>
2025-06-13 11:38:12 +1000

    Apply single edit more correctly

--- src/EditMultipleTransactions.svelte
+++ src/EditMultipleTransactions.svelte
@@ -4,7 +4,7 @@
     import {onMount} from "svelte"
     import Select from './Select.svelte'
     import Icon from '@iconify/svelte'
-    import {page} from './page.js'
+    import {isSingleEditMode, page} from './page.js'
     import {accounts} from './accounts.js'
     import {config, dateFormat} from './config.js'
     import { invoke } from "@tauri-apps/api/core"
@@ -56,16 +56,24 @@
 
     const applyChanges = (entry, changeEntry, index, errors) => {
 
-        if (changeEntry.description && changeEntry.description.length > 0) {
-            entry.description = changeEntry.description
+        const updateDescription = (changeEntry) => {
+            if (changeEntry.description && changeEntry.description.length > 0) {
+                entry.description = changeEntry.description
+            }
         }
 
-        if (changeEntry.account && changeEntry.account.id) {
-            entry.account_id = changeEntry.account.id
-            entry.entry_type = changeEntry.entry_type
+        if (compoundMode) {
+            updateDescription(entries[index])
+        } else {
+            updateDescription(entries[0])
         }
 
-        console.log(entry, changeEntry)
+        if (entries[index].account && entries[index].account.id) {
+            entry.account_id = entries[index].account.id
+            entry.entry_type = entries[index].entry_type
+        }
+
+        console.log(entry, entries[index])
     }
 
     const needSecondEntry = (transaction) =>  {
