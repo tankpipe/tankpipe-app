291a016c6ec9c2d829288ecb6b478d5f713c141f
Kremmen <you@example.com>
2025-10-30 11:40:23 +1000

    attempt to fix flakey locale in tests

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,7 @@
-import { register, init, getLocaleFromNavigator } from 'svelte-i18n'
+import { register, init, getLocaleFromNavigator, locale } from 'svelte-i18n'
 
 register('en', () => import('./locales/en.json'))
+locale.set('en')
 
 init({
   fallbackLocale: 'en',
