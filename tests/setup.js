68e9c340585b137c8b16737d0abef0a3044460f0
Kremmen <you@example.com>
2026-04-18 09:03:46 +1000

    set locale before each test run

--- tests/setup.js
+++ tests/setup.js
@@ -1,7 +1,16 @@
 import { cleanup } from '@testing-library/svelte'
-import { afterEach } from 'vitest'
+import { afterEach, beforeEach } from 'vitest'
+import { locale, waitLocale } from 'svelte-i18n'
+import '../src/utils/i18n.js'
+
+locale.set('en')
+await waitLocale()
+
+beforeEach(async () => {
+  locale.set('en')
+  await waitLocale()
+})
 
 afterEach(() => {
   cleanup()
 })
-
